246edc1e1f5f44be645a1a7c1f3cd6163a71437c
Daniel Suárez <dwsuarez98@gmail.com>
2022-07-14 15:09:51 -0300

    Formato Agregado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #URL IMDB favorites of the moments
-url = "https://www.imdb.com/chart/moviemeter/?ref_=watch_tpks_chtmvm"
+URL = "https://www.imdb.com/chart/moviemeter/?ref_=watch_tpks_chtmvm"
 
 
 class Extract:
@@ -10,27 +10,31 @@ class Extract:
         pass
     
 
-    def soup(self,url):
-        page = requests.get(url)
+    def soup(self,URL):
+        page = requests.get(URL)
         soup = BeautifulSoup(page.content,"html.parser")
         return soup
 
     
     def favorites_imdb(self):
-        soup = self.soup(url)
+        soup = self.soup(URL)
         content_fav = soup.find_all('td',attrs={'class':'titleColumn'})
         cont = 1
 
         for i in content_fav:
             sub = i.find('a').text
             print(f'{cont} ',sub)
-            if cont == 21:
+            if cont == 20:
                 break
             cont += 1
 
 
+    def format_info(self):
+        print(f"{'{ Recomended show }':=^50}")
+        self.favorites_imdb()
+
 
 if __name__ == '__main__':
     cl = Extract()
-    cl.favorites_imdb()
+    cl.format_info()
                 
